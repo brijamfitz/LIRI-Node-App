d56b723f195b893d5c22f2e40540df30772250d8
brijamfitz <brijamfitz@Brians-MacBook-Pro.local>
2018-10-05 02:29:30 -0400

    work in progress

--- liri.js
+++ liri.js
@@ -3,40 +3,44 @@ require("dotenv").config();
 // GLOBAL VARIABLES
 // =============================================================================
 var nodeArgs = process.argv;
-// console.log(nodeArgs);
 var commandArg = process.argv[2];
-// console.log(commandArg);
 var argsArray = [];
 
 for (var i = 3; i < nodeArgs.length; i++) {
     argsArray.push(nodeArgs[i]);
 }
-// console.log(argsArray);
 
 var userInput = argsArray.join(' ');
-// console.log(song);
-
 
 // SPOTIFY
 // spotify-this-song
 // =============================================================================
 // Import keys.js file and store in variable
 var keys = require('./keys.js');
-// console.log(keys);
-
 var Spotify = require('node-spotify-api');
-
 var spotify = new Spotify(keys.spotify);
-// console.log(spotify);
 
 function spotifyThisSong() {
   spotify.search({ type: 'track', limit: 1, query: userInput }, function(err, data) {
-      if (err) {
-        return console.log('Error occurred: ' + err);
-      }
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    var artistName = data.tracks.items[0].album.artists[0].name;
+    var songTitle = data.tracks.items[0].name;
+    var albumTitle = data.tracks.items[0].album.name;
+    var songPreview = data.tracks.items[0].preview_url;
     
-    // console.log(JSON.stringify(data, null, 2));
-
+    console.log('The name of the artist is: ' + artistName  + '.');
+    console.log('The name of the song is: ' + songTitle  + '.');
+    console.log('The name of the album is: ' + albumTitle  + '.');
+    console.log('Follow this link for a preview of the song: ' + songPreview);
+  });
+}
+function spotifyDefault() {
+  spotify.search({ type: 'track', limit: 1, query: 'Ace of Base' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
     var artistName = data.tracks.items[0].album.artists[0].name;
     var songTitle = data.tracks.items[0].name;
     var albumTitle = data.tracks.items[0].album.name;
@@ -62,11 +66,9 @@ function concertThis() {
         console.log('error:', error); 
       }
       // Print the response status code if a response was received
-      // console.log('statusCode:', response && response.statusCode); 
+      console.log('statusCode:', response && response.statusCode); 
 
       var body = JSON.parse(body);
-      // console.log('body:', body);
-
       var venueName = body[0].venue.name;
       var venueCity = body[0].venue.city + ', ' + body[0].venue.region;
       var concertDate = body[0].datetime.split('T');
@@ -92,18 +94,30 @@ function movieThis() {
     console.log('statusCode:', response && response.statusCode); 
 
     var body = JSON.parse(body);
-    // console.log('body:', body);
 
-    // Title of the movie.
-    // Year the movie came out.
-    // IMDB Rating of the movie.
-    // Rotten Tomatoes Rating of the movie.
-    // Country where the movie was produced.
-    // Language of the movie.
-    // Plot of the movie.
-    // Actors in the movie.
+    console.log(body.Title);
+    console.log('It was released in ' + body.Year  + '.');
+    console.log('It was directed by ' + body.Director + '.');
+    console.log('IMDB gives it a ' + body.imdbRating + ' out of 10.');
+    console.log('Rotten Tomatoes gives it a ' + body.Ratings[1].Value + ' rating.');
+    console.log('It was produced in ' + body.Country  + '.');
+    console.log('The language(s) is ' + body.Language  + '.');
+    console.log('The cast includes ' + body.Actors  + '.');
+    console.log('And here is a short summary of the plot: ' + body.Plot);
+  });
+}
+function movieDefault() {
+  request('http://www.omdbapi.com/?t=' + 'Mr. Nobody' + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
+    if (error) {
+      // Print the error if one occurred  
+      console.log('error:', error); 
+    }
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode); 
 
-    console.log('The title of the movie is ' + body.Title + '.');
+    var body = JSON.parse(body);
+
+    console.log(body.Title);
     console.log('It was released in ' + body.Year  + '.');
     console.log('It was directed by ' + body.Director + '.');
     console.log('IMDB gives it a ' + body.imdbRating + ' out of 10.');
@@ -119,49 +133,63 @@ function movieThis() {
 // =============================================================================
 var fs = require('fs');
 
-fs.readFile('random.txt', 'utf8', function(error, data) {
-  if (error) {
-    console.log(error);
-  }
+function doWhatItSays() {
+  fs.readFile('random.txt', 'utf8', function(error, data) {
+    if (error) {
+      console.log(error);
+    }
 
-  dataSplit = data.split(',');
-  // console.log(dataSplit);
+    var dataSplit = data.split(',');
+    var quoteSplit = dataSplit[1].split('"');
+    userInput = quoteSplit[1];
 
-  // console.log(dataSplit[0]);
-  // console.log(dataSplit[1]);
-
-  if (commandArg === 'do-what-it-says') {
-    var array = process.argv;
-    array.push(dataSplit[0], dataSplit[1]);
-  }
-})
-
+    if (dataSplit[0] === 'spotify-this-song') {
+      spotifyThisSong();
+    }
+    else if (dataSplit[0] === 'movie-this') {
+      movieThis();
+    }
+    else if (dataSplit[0] === 'concert-this') {
+      concertThis();
+    }
+  })
+}
 
 // FUNCTION CALLS
 // =============================================================================
-switch (commandArg) {
-  case 'spotify-this-song':
-  spotifyThisSong();
-  break;
-
-  case 'concert-this':
-  concertThis();
-  break;
-
-  case 'movie-this':
-  movieThis();
-  break;
-}
-
-// if (commandArg === 'spotify-this-song') {
+// switch (commandArg) {
+//   case 'spotify-this-song':
 //   spotifyThisSong();
-// }
-// else if (commandArg === 'concert-this') {
+//   break;
+
+//   case 'concert-this':
 //   concertThis();
-// }
-// else if (commandArg === 'movie-this') {
+//   break;
+
+//   case 'movie-this':
 //   movieThis();
-// }
-// else if (commandArg === 'do-what-it-says') {
+//   break;
+
+//   case 'do-what-it-says':
 //   doWhatItSays();
+//   break;
 // }
+
+if (commandArg === 'spotify-this-song' && argsArray.length >= 1) {
+  spotifyThisSong();
+}
+else if (commandArg === 'spotify-this-song' && argsArray.length === 0) {
+  spotifyDefault();
+}
+else if (commandArg === 'movie-this' && argsArray.length >= 1) {
+  movieThis();
+}
+else if (commandArg === 'movie-this' && argsArray.length === 0) {
+  movieDefault();
+}
+else if (commandArg === 'concert-this' && argsArray.length >= 1) {
+  concertThis();
+}
+else if (commandArg === 'do-what-it-says') {
+  doWhatItSays();
+}
